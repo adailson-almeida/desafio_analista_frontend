0490b215e5fb525a05bc5e4cc773c7e7c6ee0215
Adailson Almeida <57320221+adailson-almeida@users.noreply.github.com>
2025-12-13 20:09:59 -0300

    feat: implementação da tabela de listagem

--- gerenciador-de-chamados/src/app/features/tickets/pages/ticket-list/ticket-list.component.ts
+++ gerenciador-de-chamados/src/app/features/tickets/pages/ticket-list/ticket-list.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { SelectModule } from 'primeng/select';
+
+interface Chamado {
+  id: number;
+  titulo: string;
+  descricao: string;
+  categoria: string;
+}
 
 @Component({
   selector: 'app-ticket-list',
   standalone: true,
-  imports: [CommonModule, CardModule], // Importe CommonModule e componentes PrimeNG
+  imports: 
+  [
+    CommonModule, 
+    CardModule,
+    TableModule,
+    SelectModule
+  ],
   templateUrl: './ticket-list.component.html',
   styleUrls: ['./ticket-list.component.css']
 })
@@ -17,7 +32,12 @@ export class TicketListComponent implements OnInit {
 
   ngOnInit(): void {
     
-    console.log('Componente de Listagem de Chamados carregado!');
+    this.chamados = [
+      { id: 1, titulo: 'Impressora não funciona', descricao: 'A impressora HP no 3º andar não liga de jeito nenhum. Já verifiquei o cabo de energia.', categoria: 'Hardware' },
+      { id: 2, titulo: 'Erro ao logar no sistema X', descricao: 'Ao tentar acessar o sistema X com meu usuário padrão, recebo a mensagem "Usuário ou senha inválidos".', categoria: 'Software' },
+      { id: 3, titulo: 'Internet lenta na sala 5', descricao: 'A velocidade da conexão de rede na sala 5 está muito abaixo do normal nas últimas 2 horas.', categoria: 'Rede' },
+      { id: 4, titulo: 'Solicitação de novo monitor', descricao: 'Preciso de um monitor extra para minha estação de trabalho, o atual é muito pequeno.', categoria: 'Hardware' }
+    ];
   }
 
 }
