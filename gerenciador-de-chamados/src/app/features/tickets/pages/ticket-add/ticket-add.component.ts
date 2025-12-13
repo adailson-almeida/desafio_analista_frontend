804bc45d2761bb883e55f457aa847d11f2d4b3e9
Adailson Almeida <57320221+adailson-almeida@users.noreply.github.com>
2025-12-13 00:19:10 -0300

    feat: ajustes no form.

--- gerenciador-de-chamados/src/app/features/tickets/pages/ticket-add/ticket-add.component.ts
+++ gerenciador-de-chamados/src/app/features/tickets/pages/ticket-add/ticket-add.component.ts
@@ -4,6 +4,15 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectModule } from 'primeng/select';
+import { 
+  FormBuilder, 
+  FormGroup, 
+  Validators, 
+  ReactiveFormsModule // Importe este módulo
+} from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-add',
@@ -13,7 +22,11 @@ import { FormsModule } from '@angular/forms';
     CardModule,
     InputTextModule,
     ButtonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    SelectModule
   ],
   templateUrl: './ticket-add.component.html',
   styleUrls: ['./ticket-add.component.css']
@@ -22,16 +35,36 @@ export class TicketAddComponent implements OnInit {
 
   tituloChamado: string = '';
 
-  constructor() { }
+  ticketForm!: FormGroup;
+  categorias: any[] = [];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.categorias = [
+      { label: 'Hardware', value: 'HARDWARE' },
+      { label: 'Software', value: 'SOFTWARE' },
+      { label: 'Rede', value: 'NETWORK' },
+      { label: 'Outro', value: 'OTHER' }
+    ];
+
+    this.ticketForm = this.fb.group({
+      titulo: ['', Validators.required],
+      descricao: ['', Validators.required],
+      categoria: [null, Validators.required]
+    });
     console.log('Componente de Adição de Chamados carregado!');
   }
 
   salvarChamado() {
-    console.log('Chamado a ser salvo:', this.tituloChamado);
-    alert(`Salvando chamado com título: ${this.tituloChamado}`);
-    // Lógica para enviar para o serviço/API (faremos isso mais tarde)
+    if (this.ticketForm.valid) {
+      console.log('Chamado a ser salvo:', this.ticketForm.value);
+      alert('Chamado salvo com sucesso! (Verifique o console para os dados)');
+      // Lógica para enviar para o serviço/API (faremos isso mais tarde)
+      this.ticketForm.reset(); // Limpa o formulário após salvar
+    } else {
+      alert('Por favor, preencha todos os campos obrigatórios.');
+    }
   }
 
 }
