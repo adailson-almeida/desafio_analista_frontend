0633ee11b26df9b59a42793fb06c55c253b300e5
Adailson Almeida <57320221+adailson-almeida@users.noreply.github.com>
2025-12-14 12:40:18 -0300

    fix: ajuste no service, lógica para salvar no local storage do browser.

--- gerenciador-de-chamados/src/app/features/tickets/services/ticket.service.ts
+++ gerenciador-de-chamados/src/app/features/tickets/services/ticket.service.ts
@@ -7,15 +7,41 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 })
 export class TicketService {
 
-    private chamadosSimulados: Chamado[] = [
-    { id: 1, titulo: 'Impressora não funciona', descricao: 'A impressora HP no 3º andar não liga.', categoria: 'Hardware' },
-    { id: 2, titulo: 'Erro de Login ERP', descricao: 'Ao tentar acessar o sistema X, recebo erro.', categoria: 'Software' }
-  ];
+    private readonly STORAGE_KEY = 'chamados_data';
+
+    private chamadosSimulados: Chamado[] = [];
 
   private chamadosSubject = new BehaviorSubject<Chamado[]>(this.chamadosSimulados);
   public chamados$: Observable<Chamado[]> = this.chamadosSubject.asObservable();
 
-  constructor() { }
+  constructor() { 
+    this.loadInitialData();
+  }
+
+  private loadInitialData(): void {
+
+    const data = localStorage.getItem(this.STORAGE_KEY);
+
+    if (data){
+        this.chamadosSimulados = JSON.parse(data);
+    } else {
+        this.chamadosSimulados = this.getMockData();
+        this.saveToLocalStorage();
+    }
+    
+    this.chamadosSubject.next(this.chamadosSimulados);
+  }
+
+  private saveToLocalStorage(): void {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.chamadosSimulados));
+  }
+
+  private getMockData(): Chamado[] {
+    return[
+      { id: 1, titulo: 'Impressora não funciona', descricao: 'A impressora HP no 3º andar não liga.', categoria: 'Hardware' },
+      { id: 2, titulo: 'Erro de Login ERP', descricao: 'Ao tentar acessar o sistema X, recebo erro.', categoria: 'Software' }
+    ];
+  }
 
   addChamado(novoChamado: Omit<Chamado, 'id'>): Observable<Chamado> {
     const novoId = this.chamadosSimulados.length + 1;
@@ -25,6 +51,9 @@ export class TicketService {
     };
 
     this.chamadosSimulados.push(chamadoCompleto);
+
+    this.saveToLocalStorage();
+
     this.chamadosSubject.next(this.chamadosSimulados)
 
     return of(chamadoCompleto);
